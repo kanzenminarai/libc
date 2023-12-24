456752d3b0a57eeee87f536cf2bd7cafebdb509a
Kanzen <kanzenminarai@proton.me>
2023-12-23 20:10:53 -0400

    added source code

--- src/string/stringCompareSize.c
+++ src/string/stringCompareSize.c
@@ -1,1 +1,8 @@
+#include "string.h"
 
+int stringCompareSize(char *target, char *source, unsigned int size) {
+  unsigned int counter = 0, comparedStrings = 0;
+  while(target[counter] == source[counter] && target[counter]) if(counter < size) counter++;
+  comparedStrings = target[counter] - source[counter];
+  return comparedStrings;
+}
