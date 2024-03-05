0d9f1da4adb5ae0f09c5393ed3dbea7ab61ce923
kanzen minarai <kanzenminarai@proton.me>
2024-03-04 21:16:21 -0400

    added new functions

--- include/string.h
+++ include/string.h
@@ -1,13 +1,7 @@
-#ifndef STRING_H
-#define STRING_H
+#pragma once
 
-#define NULL_PTR (void*)0
-#define NULL_TERMINATOR '\0'
-
-int asciiToInteger(char *string);
+int stringToInteger(char *string);
 unsigned int stringLength(char *string);
 int stringCompare(char *target, char *source);
 char* stringCopy(char *target, char *source);
 char* stringCopySize(char *target, char *source, unsigned int size);
-
-#endif // STRING_H
