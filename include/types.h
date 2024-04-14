cf4d927f1292365ad51b710009ee607fd45271bc
kanzen minarai <kanzenminarai@proton.me>
2024-04-13 20:50:30 -0400

    added different types into the project (not definitive)

---
+++ include/types.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/* Integer */
+// Unsigned 
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
+
+
+// Signed
+typedef char i8;
+typedef short int i16;
+typedef int i32;
+typedef long int i64;
+
+
+/* String */
+typedef char* str;
+
+/* Floating point */
+typedef float f32;
+typedef double f64;
+
